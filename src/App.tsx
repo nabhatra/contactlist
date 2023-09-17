3d04856331c38ed0e5703c933d593fd08626ec6c
nabhatra <nabhatra@yahoo.com>
2023-09-17 07:28:37 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import React, { useEffect } from "react";
+import "./App.css";
+import "./index.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Auth from "./pages/Auth";
+import Dashboard from "./pages/Dashboard";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import { useAppDispatch } from "./app/hooks";
+import { setUser } from "./features/authSlice";
+import PrivateRoute from "./components/PrivateRoute";
+
+function App() {
+  const dispatch = useAppDispatch();
+  const user = JSON.parse(localStorage.getItem("user") || "{}");
+
+  useEffect(() => {
+    dispatch(setUser(user));
+  }, [dispatch, user]);
+
+  return (
+    <div className="App">
+      <Provider store={store}>
+        <BrowserRouter>
+          <ToastContainer />
+          <Routes>
+            <Route path="/" element={<Navigate to="/auth" replace />} />
+            <Route path="/auth" element={<Auth />} />
+            <Route
+              path="/dashboard"
+              element={
+                <PrivateRoute>
+                  <Dashboard />
+                </PrivateRoute>
+              }
+            />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
